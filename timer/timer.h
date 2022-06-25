24008a86e50c8acddff0e29f58f4b0f23cda5bdc
chen <1519577984@qq.com>
2022-06-25 22:27:35 +0800

    Initial commit

---
+++ timer/timer.h
@@ -0,0 +1,22 @@
+//
+// Created by chen on 2022/6/25.
+//
+
+#ifndef MYWEBSERVER_TIMER_H
+#define MYWEBSERVER_TIMER_H
+
+struct client_data{
+	int sock_fd;
+
+};
+
+class timer {
+
+};
+
+class heap_time{
+
+};
+
+
+#endif //MYWEBSERVER_TIMER_H
